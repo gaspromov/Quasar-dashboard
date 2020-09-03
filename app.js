971246014308948dbe51c811c5b6c646c8568848
Vyacheslav Borisov <slava2bb@gmail.com>
2020-09-03 21:14:20 +0300

    Fixed cors

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ app.set('views', __dirname + '/views');
 
 app.use(helmet());
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+});
 
 app.use('/auth', authRoute);
 
