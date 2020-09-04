6160a7f6944e18bd063a494d1fe315e1c779f62a
Gaspromov <gaspromov@mail.ru>
2020-09-04 21:39:01 +0300

    Вывод респонса

--- client/src/app/main/navbar/navbar.component.ts
+++ client/src/app/main/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/auth/auth.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,25 +10,40 @@ import { AuthService } from 'src/app/shared/auth/auth.service';
 export class NavbarComponent implements OnInit {
 
   constructor(
-    private http: AuthService
-  ) { }
+    private http: AuthService,
+    private activatedRoute: ActivatedRoute,
+  ) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      console.log(params);
+    })
+   }
 
   ngOnInit(): void {
   }
 
   async auth(){
     // window.location.href = ''
-    await this.http.auth()
-    .then( (w: any ={}) => {
-      console.log("это не ошибка", w);
-      // window.location.href =
-      // console.log(w.error);
-    })
-    .catch(e =>{
-      console.log("это ошибка", e);
-      // window.location.href =  e.error.text;
+    // await this.http.auth()
+    // .then( w => {
+    //   console.log("это не ошибка", w);
+    //   // window.location.href =
+    //   // console.log(w.error);
+    // })
+    // .catch(e =>{
+    //   console.log("это ошибка", e);
+    //   // window.location.href =  e.error.text;
 
-    })
+    // })
+    
+    
+    fetch('http://localhost:3001/login/discord', {
+    method: 'GET',
+    headers: {
+    'content-type': 'application/json',
+    }
+    }).then(result => console.log(result))
+    // .then(console.log);
+    
   }
 
 }
