584ccb000d31244aa7302cf51f8135693a3279f1
gaspromov <gaspromov@mail.ru>
2020-10-02 18:18:31 +0300

    Фикс

--- client/src/app/main/main/main.component.ts
+++ client/src/app/main/main/main.component.ts
@@ -62,9 +62,6 @@ export class MainComponent implements OnInit {
 
   makeValidURL(){
     if (this.successes != undefined){
-      for (let i = 0; i < this.successes.length; i++){
-        this.successes[i].image = this.successes[i].image;
-      }
       this.successes.reverse()
     }
   }
