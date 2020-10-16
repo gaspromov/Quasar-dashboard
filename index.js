4bc2e88bc89cc25ecc65268b75cc6098cc8e77e7
Maxim Khotsevich <xozewitc@yandex.ru>
2020-10-16 22:48:04 +0300

    кук

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // Middleware
+app.use(cors())
 app.use(
 	fileUpload({
 		createParentPath: true,
@@ -43,7 +44,6 @@ app.use(express.json())
 app.use(compression())
 app.use(morgan('dev'))
 app.use(helmet())
-app.use(cors())
 
 const sess = {
 	name: 'discord.oauth2',
